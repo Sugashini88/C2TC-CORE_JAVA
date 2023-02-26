779c911cc73ce39860430afb9c382c295695558c
ELCOT <ELCOT@HP>
2023-02-26 20:36:07 +0530

    commit

---
+++ sugashini/src/sugashini/DecisionmakingWithOperator.java
@@ -0,0 +1,15 @@
+package sugashini;
+
+public class DecisionmakingWithOperator {
+
+	public static void main(String[] args) {
+	 int a=10;
+	 int b=20;
+	 
+	 if((a<b) || (a==b))
+	   System.out.println("the condition is TRUE ");
+	 else 
+		 System.out.println("the condition is FALSE");
+	}
+
+}
---
+++ sugashini/src/sugashini/ForEachLoop.java
@@ -0,0 +1,23 @@
+package sugashini;
+
+public class ForEachLoop {
+
+	public static void main(String[] args) {
+		int a[]= {1,2,3,4,5,6};
+		for(int p : a) {
+			System.out.println(p);
+			
+		}
+		char b[]= {'s','u','g','a','s','h','i','n','i'};
+		for(char r:b) {
+			System.out.println(r);
+		
+		}
+		String s1[]= {"peri","college"};
+		for(String v:s1) {
+			System.out.println(v);
+		}
+		
+	}
+
+}
